b3e6f0e86ff7b1a295d62f8c9404619bb816c5b4
vikash <imvikky31@gmail.com>
2024-04-18 17:18:15 +0530

    Base url updated

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import "./Login.css";
 
-const BASE_URL = "https://galaxy-backend-one.vercel.app";
+const BASE_URL = "https://web-crafter-backend.vercel.app/";
 // const BASE_URL = "https://galaxy-backend-49wa.onrender.com"
 
 function Login() {
--- frontend/src/Components/Register.jsx
+++ frontend/src/Components/Register.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./Register.css";
 
-const BASE_URL = "https://galaxy-backend-one.vercel.app";
+const BASE_URL = "https://web-crafter-backend.vercel.app/";
 // const BASE_URL = "https://galaxy-backend-49wa.onrender.com"
 
 function Register() {
